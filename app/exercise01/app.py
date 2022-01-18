85ac2a8ceee9372faabe1bfe9e253f47deff708e
Lucas S. Nogueira <lucas.lambda.101@gmail.com>
2022-01-18 14:43:45 -0300

    Finish 1st exercise.

--- app/exercise01/app.py
+++ app/exercise01/app.py
@@ -1,28 +1,45 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, HTTPException, status
 
 app = FastAPI()
 
 
-messages = {"message_00": "Hello, World!"}
+messages = {"message_0": "Hello, World!"}
+
 
 @app.get("/")
 def get_messages():
+    if not messages:
+        raise HTTPException(status_code=404, detail="No message found!")
+    
     return messages
+
 
 @app.post("/message", status_code=status.HTTP_201_CREATED)
 def create_message(message: str):
-    currently_index = len(messages) + 1 
-    next_key_index = f"message_{currently_index}"
+    current_index = len(messages) + 1
+    next_key_index = f"message_{current_index}"
     messages[next_key_index] = message
-    return f"Message number {currently_index} Created!"
+    
+    return f"Message number {current_index} Created!"
+
 
 @app.put("/message/{id}")
 def update_message(id: int, message: str):
     this_key = f"message_{id}"
-    messages.update(this_key + ":" + message)
+
+    if not this_key in messages:
+        raise HTTPException(status_code=404, detail=f"The message number {id} doesn't exists!")
+    
+    messages.update({this_key: message})
     return f"Message number {id} Updated!"
+
 
 @app.delete("/message/{id}")
 def delete_message(id: int):
-    messages.pop(id)
+    this_key = f"message_{id}"
+
+    if not this_key in messages:
+        raise HTTPException(status_code=404, detail=f"The message number {id} doesn't exists. So it cannot be deleted!")
+    
+    messages.pop(this_key)
     return f"Message number {id} Deleted!"
